ba31db46ae858c9da2149999a928e9dcd5f42af5
Paul <84129217+DRIVENpol@users.noreply.github.com>
2025-10-31 01:47:01 +0200

    Add Modals And Custom Notifications

--- app/provider/providers.tsx
+++ app/provider/providers.tsx
@@ -2,17 +2,20 @@ import { SafeWalletProvider } from "./SafeWalletProvider";
 import { SafeTxProvider } from "./SafeTxProvider";
 import { WagmiConfigProvider } from "./WagmiConfigProvider";
 import { WalletConnectProvider } from "./WalletConnectProvider";
+import ToastProvider from "./ToastProvider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiConfigProvider>
-      <SafeWalletProvider>
-        <SafeTxProvider>
-          <WalletConnectProvider>
-            {children}
-          </WalletConnectProvider>
-        </SafeTxProvider>
-      </SafeWalletProvider>
-    </WagmiConfigProvider>
+    <ToastProvider>
+      <WagmiConfigProvider>
+        <SafeWalletProvider>
+          <SafeTxProvider>
+            <WalletConnectProvider>
+              {children}
+            </WalletConnectProvider>
+          </SafeTxProvider>
+        </SafeWalletProvider>
+      </WagmiConfigProvider>
+    </ToastProvider>
   );
 }
